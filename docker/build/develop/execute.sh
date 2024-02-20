c000946bb52407200f8a4ed19eeea322ce3c7315
Dedy Irawan inc <dedy.irawan.inc@gmail.com>
2024-02-20 23:51:32 +0700

    240220@2351 docker scripts files

---
+++ docker/build/develop/execute.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo -e "\nBase Directory and Filename..."
+BASEDIR00="$( cd "$( dirname "$0" )" && pwd )"
+CURRDIR00=${BASEDIR00##*/}
+FILENAME=`basename $0`; FILE=$(echo $FILENAME | cut -d '.' -f 1)
+echo [$BASEDIR00] [$CURRDIR00] [$FILENAME] [$FILE]
+echo "......."
+BASEDIR01=${BASEDIR00%/*}; CURRDIR01=${BASEDIR01##*/}
+BASEDIR02=${BASEDIR01%/*}; CURRDIR02=${BASEDIR02##*/}
+BASEDIR03=${BASEDIR02%/*}; CURRDIR03=${BASEDIR03##*/}
+echo [$BASEDIR01] [$CURRDIR01]
+echo [$BASEDIR02] [$CURRDIR02]
+echo [$BASEDIR03] [$CURRDIR03]
+echo "......."
+cd $BASEDIR00 && cd ../../../
+BUILD="Dockerfile"
+COMPA=$(echo $FILE | cut -d '-' -f 1)
+GROUP=$(echo $FILE | cut -d '-' -f 2)
+IMAGE=ghcr.io/dedycatedinc/${CURRDIR03//"-"/"_"} && echo $IMAGE
+if [ "$COMPA" != "$GROUP" ]; then
+  BUILD=${BUILD}-${GROUP}
+  IMAGE=${IMAGE}_${GROUP}:${CURRDIR00}
+else
+  IMAGE=${IMAGE}:${CURRDIR00}
+fi
+echo [$BUILD] [$CURRDIR00] [$GROUP] [$IMAGE]
+rm ./${BUILD}
+cp ./docker/build/${CURRDIR00}/${BUILD} ./${BUILD}
+cat ./${BUILD}
+ls -al
+# ERROR [internal] load metadata for docker.io
+if [ -f ~/.docker/config.json ]; then
+  sudo rm  ~/.docker/config.json
+else
+  sudo mkdir ~/.docker
+fi
+sudo cp ./docker/config.json ~/.docker/config.json
+sudo chmod 755  ~/.docker/config.json
+sudo docker build --file $BUILD --rm --tag $IMAGE .
+sudo docker rmi -f $(sudo docker images | grep "<none>" | awk "{print \$3}")
+sudo docker images
+exit 0
---
+++ docker/build/main/upload.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo -e "\nBase Directory and Filename..."
+BASEDIR00="$( cd "$( dirname "$0" )" && pwd )"
+CURRDIR00=${BASEDIR00##*/}
+FILENAME=`basename $0`; FILE=$(echo $FILENAME | cut -d '.' -f 1)
+echo [$BASEDIR00] [$CURRDIR00] [$FILENAME] [$FILE]
+echo "......."
+BASEDIR01=${BASEDIR00%/*}; CURRDIR01=${BASEDIR01##*/}
+BASEDIR02=${BASEDIR01%/*}; CURRDIR02=${BASEDIR02##*/}
+BASEDIR03=${BASEDIR02%/*}; CURRDIR03=${BASEDIR03##*/}
+echo [$BASEDIR01] [$CURRDIR01]
+echo [$BASEDIR02] [$CURRDIR02]
+echo [$BASEDIR03] [$CURRDIR03]
+echo "......."
+cd $BASEDIR00 && cd ../../
+COMPA=$(echo $FILE | cut -d '-' -f 1)
+GROUP=$(echo $FILE | cut -d '-' -f 2)
+IMAGE=ghcr.io/dedycatedinc/${CURRDIR03//"-"/"_"} && echo $IMAGE
+if [ "$COMPA" != "$GROUP" ]; then
+  IMAGE=${IMAGE}_${GROUP}:${CURRDIR00}
+else
+  IMAGE=${IMAGE}:${CURRDIR00}
+fi
+echo [$CURRDIR00] [$GROUP] [$IMAGE]
+PASSWORD=$(< ./password.txt)
+USERNAME=$(< ./username.txt)
+echo [$USERNAME] [$PASSWORD]
+TYPE=$(uname -s | cut -d '_' -f 1) && echo [$TYPE]
+if [ "$TYPE" == "Darwin" ]; then
+  security -v unlock-keychain ~/Library/Keychains/login.keychain-db
+fi
+echo $PASSWORD | sudo docker login ghcr.io -u $USERNAME --password-stdin
+sudo docker push $IMAGE
+exit 0
