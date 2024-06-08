69e1e3ea2c1dc49995be3d50e3a5c830046c72fc
Dedy Irawan inc <dedy.irawan.inc@gmail.com>
2024-06-08 14:06:39 +0700

    240608@1406 docker file updates

--- docker/compose/execute.sh
+++ docker/compose/execute.sh
@@ -6,13 +6,15 @@ FILENAME=`basename $0`; FILE=$(echo $FILENAME | cut -d '.' -f 1)
 echo [$BASEDIR00] [$CURRDIR00] [$FILENAME] [$FILE]
 echo "......."
 cd $BASEDIR00
-VERTICAL="\nID\t\t{{.ID}}\nImage\t\t{{.Image}}\nCommand\t\t{{.Command}}\nCreated\t\t{{.RunningFor}}\nStatus\t\t{{.Status}}\nPorts\t\t{{.Ports}}\nNames\t\t{{.Names}}\n"
+BRANCH=$(git rev-parse --abbrev-ref HEAD) && echo [$BRANCH]
+cd $BRANCH
+FORMAT="\nID\t\t{{.ID}}\nImage\t\t{{.Image}}\nCommand\t\t{{.Command}}\nCreated\t\t{{.RunningFor}}\nStatus\t\t{{.Status}}\nPorts\t\t{{.Ports}}\nNames\t\t{{.Names}}\n"
 if [ "$TYPE" == "Darwin" ]; then
   docker-compose up -d --force-recreate
-  docker ps -a --format=$VERTICAL
+  docker ps -a --format=$FORMAT
 else
   sudo docker-compose up -d --force-recreate
-  sudo docker ps -a --format=$VERTICAL
+  sudo docker ps -a --format=$FORMAT
 fi
 nmap -p 3010 localhost
 nmap -p 3011 localhost
